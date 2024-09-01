cb769f78c047bbdd5889406f10b6cbc7266d579c
shahriar <shahriarksaurov@gmail.com>
2024-09-01 23:34:28 +0200

    frontend initial

---
+++ frontend/src/app/index.js
@@ -0,0 +1,28 @@
+
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import exercises from '../../assets/data/exercises.json';
+import ExerciseListItem from '../components/ExerciseListItem';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={exercises}
+        contentContainerStyle={{gap:5}}
+        renderItem={({ item }) => (
+          <ExerciseListItem item={item} />
+        )}
+        keyExtractor={(item, index) => item.name + index}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'ghostwhite',
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
